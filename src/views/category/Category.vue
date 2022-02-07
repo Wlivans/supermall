a03b0d5e387f5197f32cee7f28183d6648cae6cc
Marile <1587945736@qq.com>
2022-02-07 20:19:27 +0800

    首页组件化

---
+++ src/views/category/Category.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>分类</div>
+</template>
+
+<script>
+// import { Button } from 'vant'; // 根据vant的引入即可
+// import { getData, addListData } from 'network/demoRuquest'; // 引入封装好的网络请求
+export default {
+  name: "Category",
+  components: {
+    // [Button.name]: Button // 此处是引入组件名字的方法
+  },
+  data() {
+    return {};
+  },
+  created() {
+    // getData()  //  请求数据
+    //   .then(res => {
+    //     console.log(res);
+    //   })
+    //   .catch(msg => console.log(msg));
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped></style>
